c82d8734785c2f81b442e57565058316acb9e216
Emrys <emrys@openenergymonitor.org>
2019-12-12 11:45:59 +0000

    empty env settings vars are now ignored

--- process_settings.php
+++ process_settings.php
@@ -26,13 +26,26 @@ if(file_exists(dirname(__FILE__)."/settings.php")) {
         //$settings_error_title = "settings.php file error";    
         //$settings_error_message = "It looks like you are using an old version of settings.php try re-creating your settings.php file from default-settings.php";
     } else {
-        $settings = array_replace_recursive($_settings,$settings);
+        $settings = array_replace_recursive($_settings, $settings);
     }
 } else if(file_exists(dirname(__FILE__)."/settings.ini")) {
+    // load .ini files [default, settings]
     $CONFIG_INI = parse_ini_file("default-settings.ini", true);
     $CUSTOM_INI = parse_ini_file("settings.ini", true);
-    $ENV_INI = parse_ini_file("settings.env.ini", true);
-    $settings = ini_check_envvars(array_replace_recursive($CONFIG_INI, $CUSTOM_INI, $ENV_INI));
+    $settings = array_replace_recursive($CONFIG_INI, $CUSTOM_INI);
+
+    // if ENV replacement available add it to the list of settings
+    if(file_exists(dirname(__FILE__)."/settings.env.ini")) {
+        $ENV_INI = parse_ini_file("settings.env.ini", true);
+        // if file correctly parsed with items replace {{vars}} with ENV vars
+        if (count($ENV_INI) > 0) {
+            // will be empty if no ENV vars match .ini file placeholders
+            $env_settings = ini_check_envvars($ENV_INI);
+        }
+        // only add the values if the ENV vars have been found
+        $settings = array_replace_recursive($settings, $env_settings);
+    }
+
 } else {
     $settings_error = true;
     $settings_error_title = "missing settings file";
@@ -61,7 +74,7 @@ if (is_dir($settings["emoncms_dir"]."/modules")) {
 
 // Set display errors
 if (isset($settings["display_errors"]) && ($settings["display_errors"])) {
-    error_reporting(E_ALL);
+    error_reporting(E_ALL ^ E_DEPRECATED);
     ini_set('display_errors', 'on');
 }
 
@@ -69,25 +82,11 @@ if (isset($settings["display_errors"]) && ($settings["display_errors"])) {
 // FUNCTIONS
 // ---------------------------------------------------------------------------------------
 
-// This function takes two arrays of settings and merges them, using
-// the value from $overrides where it differs from the one in $defaults.
-function ini_merge ($defaults, $overrides) {
-    foreach ($overrides as $k => $v) {
-        if (is_array($v)) {
-            $defaults[$k] = ini_merge($defaults[$k], $overrides[$k]);
-        } else {
-            $defaults[$k] = resolve_env_vars($v,$defaults[$k]);
-#            $defaults[$k] = $v;
-        }
-    }
-
-    return $defaults;
-};
-
 // This function iterates over all the config file entries, replacing values
 // of the format {{VAR_NAME}} with the environment variable 'VAR_NAME'.
 //
 // This can be useful in containerised setups, or testing environments.
+// if {{VAR_NAME}} found in .ini file but not an ENV variable it is removed.
 function ini_check_envvars($config) {
     global $error_out;
 
@@ -97,6 +96,7 @@ function ini_check_envvars($config) {
             // named environment var
             if(is_array($value)) {
                 foreach($value as $subKey => $subValue) {
+                    // loop through INI $value that is an array. currently not supported
                     //@todo: add array_walk_recursive() style loop but keeping the $section value
                 }
             } elseif (strpos($value, '{{') !== false && strpos($value, '}}') !== false) {
@@ -104,9 +104,11 @@ function ini_check_envvars($config) {
                 foreach ($matches[1] as $match) {
                     if (!isset($_ENV[$match])) {
                         $error_out .= "<p>Error: environment var '${match}' not defined in config section [${section}], setting '${key}'</p>";
+                        // no match found remove ENV var overide field
+                        unset($config[$section][$key]);
                     } else {
+                        echo $value;
                         $newval = str_replace('{{'.$match.'}}', $_ENV[$match], $value);
-
                         // Convert booleans from strings
                         if ($newval === 'true') {
                             $newval = true;
@@ -125,5 +127,6 @@ function ini_check_envvars($config) {
             }
         endforeach;endif;
     }
-    return $config;
+    // return only arrays with replaced ENV values
+    return array_filter($config);
 }
